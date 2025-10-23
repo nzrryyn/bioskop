d47ee7f5dfdb205b8753573471b619b5fc7009a1
nzrryyn <nizardpa@gmail.com>
2025-10-23 11:31:27 +0800

    Add files via upload

---
+++ app/tiket/KNY/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./n/c.css"
+
+
+export default function page() {
+  return (
+    <div><h1>page</h1></div>
+  )
+}
---
+++ app/tiket/sp/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+import "../KNY/n/c.css"
+
+
+function page() {
+  return (
+    <div><h1>Halo</h1></div>
+  )
+}
+
+export default page
